08d019ad55de67b1fd7428fddc624e24383de1d3
RAKHYUN <kyle2598@gmail.com>
2023-11-28 13:49:20 -0600

    Initial Commit

---
+++ GuessFlags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessFlags
+//
+//  Created by Rakhyun Kim on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessFlags/GuessFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagsApp.swift
+//  GuessFlags
+//
+//  Created by Rakhyun Kim on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
